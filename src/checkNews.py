09d726f9403529c1aa023229de38d52b9a816eb1
MaimiKomuro <71859553+MaimiKomuro@users.noreply.github.com>
2021-06-11 01:00:23 +0900

    add python script and DEV

---
+++ src/checkNews.py
@@ -0,0 +1,25 @@
+# 東京都のコロナのニュースのタイトルとそのリンク先を取得します
+
+from requests_html import HTMLSession
+import slackweb
+import re
+
+url = "https://news.yahoo.co.jp/pages/article/covid19tokyo"
+
+# セッション開始
+session = HTMLSession()
+r = session.get(url)
+
+# HTMLを生成
+r.html.render()
+
+for num in range(1, 4):
+    # スクレイピング
+    article_title = r.html.find("#tab_1 > ul.dlpThumbLink > li:nth-child(" + str(num) + ") > a > span.dlpThumbText > span:nth-child(1)", first=True)
+    article_title_text = article_title.text
+    article_link = r.html.find("#tab_1 > ul.dlpThumbLink > li:nth-child(" + str(num) + ") > a", first=True)
+    article_url = article_link.absolute_links
+
+    # slackへ投稿
+    slack = slackweb.Slack(url="https://hooks.slack.com/services/xxxxxxxxxxxxx")
+    slack.notify(text=article_title_text + '\n' + re.sub("\{|\}|'", "", str(article_url)), channel="#general", username="covid19-news-bot", icon_emoji=":eyes:", mrkdwn=True)
